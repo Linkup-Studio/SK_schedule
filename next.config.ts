69f5ac91cc4097a88911370a26f65a285b800c36
Your Name <you@example.com>
2026-04-15 22:23:50 +0900

    fix: デプロイ先をSK_scheduleに戻したためのbasePathの修正

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/SK_schedule2",
+  basePath: "/SK_schedule",
   images: {
     unoptimized: true,
   },
